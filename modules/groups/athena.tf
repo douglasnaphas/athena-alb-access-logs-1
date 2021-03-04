0622e202f97c539b63e3135a1468783d82f3a114
Douglas Naphas <douglasnaphas@gmail.com>
2021-03-04 12:55:20 -0500

    Give the athena group read access to data buckets
    I tried following these instructions:
    
    https://aws.amazon.com/premiumsupport/knowledge-center/athena-analyze-access-logs/
    
    When I got to the step:
    
    > 4.     Under Tables in the left pane, choose Preview table from the
    > menu button that's next to the table name. The data from the ALB access
    > logs is visible in the Results window.
    
    I got the error:
    
    > Your query has the following error(s):
    
    > com.amazonaws.services.s3.model.AmazonS3Exception: Access Denied
    > (Service: Amazon S3; Status Code: 403; Error Code: AccessDenied; Request
    > ID...
    
    I believe this is because the user athena-user-1 (who I was logged in as
    when I tried this) does not have read access on the buckets where the
    actual data is stored.
    
    This view is supported by this:
    
    https://aws.amazon.com/premiumsupport/knowledge-center/access-denied-athena/
    
    This change grants the athena users group read access to the specific
    buckets where I have ALB access log data.
    
    My template for read-only access to buckets is the AWS-managed policy
    AmazonS3ReadOnlyAccess.

--- modules/groups/athena.tf
+++ modules/groups/athena.tf
@@ -14,3 +14,27 @@ resource "aws_iam_group_policy_attachment" "attach_athena_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonAthenaFullAccess"
 }
 
+
+resource "aws_iam_group_policy" "read_athena_data_buckets" {
+  name = "read_athena_data_buckets_policy"
+  group = aws_iam_group.athena_group.name
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:Get*",
+        "s3:List*"
+      ],
+      "Resource": [
+        "arn:aws:s3:::douglasnaphasalbcreate-a-albaccesslogbucket9abe2c-10x8rlu9ujiaq",
+        "arn:aws:s3:::passover-lol-alb-access-logs-bucket-2"
+      ]
+    }
+  ]
+}
+EOF
+}
+
